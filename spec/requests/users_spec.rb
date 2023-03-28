79317d0eb1a087e9a59da63e82aa33a03e702cb3
Ibnballo1 <belloabdullateef035@gmail.com>
2023-03-28 04:57:23 +0100

    Add show test for users

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -9,7 +9,23 @@ RSpec.describe 'Users', type: :request do
     end
 
     it "renders 'index' template" do
-     expect(response).to render_template('index')
-   end
+      expect(response).to render_template('index')
+    end
+  end
+
+  describe 'GET /show' do
+    before(:example) do
+      @user = User.create(name: 'Ibnballo')
+    end
+
+    it 'User should be successful' do
+      get user_path(@user)
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders 'show' template" do
+      get user_path(@user)
+      expect(response).to render_template('show')
+    end
   end
 end
