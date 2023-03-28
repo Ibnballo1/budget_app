642fd91f3d0b4f2807926a29755863befaf72eb1
Ibnballo1 <belloabdullateef035@gmail.com>
2023-03-28 13:01:11 +0100

    Add authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,8 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, alert: exception.message
+  end
 end
