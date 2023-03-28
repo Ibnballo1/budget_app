ce265de4c702c551d0b847ee550d479d40ddd16a
Ibnballo1 <belloabdullateef035@gmail.com>
2023-03-28 22:12:42 +0100

    Update application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    users_path
+    user_entities_path(resource)
   end
 
   def after_sign_up_path_for(resource)
