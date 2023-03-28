cffb522cca5a89da57657cb383f077ce66ba1418
Ibnballo1 <belloabdullateef035@gmail.com>
2023-03-28 04:41:23 +0100

    Add test to user controller

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Users', type: :request do
+  describe 'GET /index' do
+    before(:example) { get users_path }
+
+    it 'User should be successful' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders 'index' template" do
+     expect(response).to render_template('index')
+   end
   end
 end
