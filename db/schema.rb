811a94f0144ddf47e1b0b513903c7174e504f447
Ibnballo1 <belloabdullateef035@gmail.com>
2023-03-29 19:48:55 +0100

    Join groups and entities

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_29_175124) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_29_184331) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_29_175124) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_entities_on_user_id"
+  end
+
+  create_table "entities_groups", id: false, force: :cascade do |t|
+    t.bigint "entity_id", null: false
+    t.bigint "group_id", null: false
   end
 
   create_table "groups", force: :cascade do |t|
