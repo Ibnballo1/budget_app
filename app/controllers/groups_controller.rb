69f3a5221c498936cbb318935a6a1be83e7c449d
Ibnballo1 <belloabdullateef035@gmail.com>
2023-03-29 14:54:45 +0100

    Remove entitiesgroups table

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,11 +1,12 @@
 class GroupsController < ApplicationController
   def index
-    @goups = Group.all
+    @groups = Group.where(user_id: current_user.id)
+    @entities_groups = EntitiesGroups.where(group_id: @groups.pluck(:id)).includes([:entity])
   end
 
   def show
     @group = Group.find_by(id: params[:id])
-    @entity_group = GroupEntity
+    @entity_group = EntityGroup.where(group_id: @group.id).includes([:entity])
   end
 
   def new
@@ -13,19 +14,12 @@ class GroupsController < ApplicationController
   end
 
   def create
-    # @group = Group.new(group_params)
-    # if @group.save
-    #   redirect_to new_group_path
-    # else
-    #   render 'new'
-    # end
-
     @group = Group.create(group_params.merge(user_id: current_user.id))
 
     respond_to do |format|
       format.html do
         if @group.save
-          redirect_to groups_path
+          redirect_to user_groups_path
         else
           redirect_to new_group_path
         end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_29_114235) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_29_134901) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,15 +21,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_29_114235) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_entities_on_user_id"
-  end
-
-  create_table "entities_groups", force: :cascade do |t|
-    t.bigint "entity_id", null: false
-    t.bigint "group_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["entity_id"], name: "index_entities_groups_on_entity_id"
-    t.index ["group_id"], name: "index_entities_groups_on_group_id"
   end
 
   create_table "groups", force: :cascade do |t|
@@ -55,7 +46,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_29_114235) do
   end
 
   add_foreign_key "entities", "users"
-  add_foreign_key "entities_groups", "entities"
-  add_foreign_key "entities_groups", "groups"
   add_foreign_key "groups", "users"
 end
