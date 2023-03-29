bc7bcac5f623dccd41619b53a95e143ecaf72886
Ibnballo1 <belloabdullateef035@gmail.com>
2023-03-29 17:41:08 +0100

    Update model

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,5 +1,6 @@
 class Entity < ApplicationRecord
   belongs_to :user
+  has_and_belongs_to_many :groups
 
   validates :name, :amount, presence: true
   validates :amount, numericality: { greater_than_or_equal_to: 0 }
