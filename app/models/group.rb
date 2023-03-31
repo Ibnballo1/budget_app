e294564c46dff7d72efdb204add5882142498acb
Ibnballo1 <belloabdullateef035@gmail.com>
2023-03-31 09:47:12 +0100

    Add pages

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,11 @@
 class Group < ApplicationRecord
   belongs_to :user
-  has_and_belongs_to_many :entities
+  has_many :group_payments, dependent: :destroy
+  has_many :payments, through: :group_payments
 
   validates :name, :icon, presence: true
+
+  def total_amount
+    payments.sum(:amount)
+  end
 end
