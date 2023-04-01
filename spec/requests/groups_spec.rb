114e01d2bd1ed32426b7d9562750de3dcec2c7d6
Ibnballo1 <belloabdullateef035@gmail.com>
2023-04-01 08:56:31 +0100

    Fix linter

--- spec/requests/groups_spec.rb
+++ spec/requests/groups_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe 'Groups', type: :request do
       @user = User.create(name: 'Ibnballo', email: 'ibnballo@gmail.com', password: '08174355135')
 
       @group = Group.create(user_id: @user.id, name: 'pizza',
-                                  icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-Qz_PXUP8Lsb3k6b6tTASlpGQYYCURKlmBA&usqp=CAU')
+                            icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-Qz_PXUP8Lsb3k6b6tTASlpGQYYCURKlmBA&usqp=CAU')
       sign_in @user
       get new_group_path
     end
